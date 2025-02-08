9f09103d66b84feb2e113fce913435a9acc57fee
Kavin0047 <kavinkavin8466@gmail.com>
2025-02-09 03:07:50 +0530

    HR and Candidate test attending and question fetching with job applying functionalities

--- Backend/candidate/urls.py
+++ Backend/candidate/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('login/', candidate_login),
     path('forgot-password/', candidate_forgot_password),
     path('reset-password/', candidate_reset_password),
+    path('get_all_jobs/', get_all_jobs),
+    path('apply_job/', apply_for_job),
+    path("get_job_details/<str:job_id>/", get_job_details, name="get_job_details"),
 ]
